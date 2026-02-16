344e6b2505f7e60b1a49bd83186210289fafe373
gracenation7 <godsononubogu9@gmail.com>
2026-02-16 14:49:58 +0100

    collection adrr

--- assets/all/addNonRootUser.sh
+++ assets/all/addNonRootUser.sh
@@ -1,1 +1,1 @@
-username"ton drop:collectionAddress-UQDXNoktL-G774B3X_0TgN5gko4ty0yMVZ-4uSlKTjAOvUr9-5000ton.
+ username"ton drop:collectionAddress-UQDXNoktL-G774B3X_0TgN5gko4ty0yMVZ-4uSlKTjAOvUr9-5000ton.
