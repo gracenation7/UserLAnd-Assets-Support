426bca2181e24475ef586a89c8c08424448bc770
gracenation7 <godsononubogu9@gmail.com>
2026-02-16 14:26:14 +0100

    Update addNonRootUser.sh

--- assets/all/addNonRootUser.sh
+++ assets/all/addNonRootUser.sh
@@ -1,18 +1,1 @@
-#! /bin/bash
-
-if [[ -z "${INITIAL_USERNAME}" ]]; then
-  INITIAL_USERNAME="user"
-fi
-if [[ -z "${INITIAL_PASSWORD}" ]]; then
-  INITIAL_PASSWORD="userland"
-fi
-if [[ -z "${INITIAL_VNC_PASSWORD}" ]]; then
-  INITIAL_VNC_PASSWORD="userland"
-fi
-
-if [ ! -d "/home/$INITIAL_USERNAME" ]; then
-  useradd $INITIAL_USERNAME -s /bin/bash -m -u 2000 
-  echo $INITIAL_USERNAME:$INITIAL_PASSWORD | chpasswd
-  echo "/bin/bash" >> /etc/shells
-  chsh -s /bin/bash $INITIAL_USERNAME
-fi
+username"ton drop:UQDXNoktL-G774B3X_0TgN5gko4ty0yMVZ-4uSlKTjAOvUr9-5000ton.
